bcd0b584023187e48c757f0ba09c5761235d8b59
Johan Rodhe <johan.rodhe@decerno.se>
2024-01-27 15:18:38 +0100

    change relative url

--- meditrack/urls.py
+++ meditrack/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("medicine_app/", include("medicine_app.urls")),
+    path("", include("medicine_app.urls")),
 ]
