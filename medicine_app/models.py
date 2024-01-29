95cbe80eb9af3f4ee29218828e0b68f35c4dc63d
Johan Rodhe <johan.rodhe@decerno.se>
2024-01-29 10:40:00 +0100

    move models to accounts

--- medicine_app/models.py
+++ medicine_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Medicine(models.Model):
     name = models.CharField(max_length=100)
@@ -26,13 +27,7 @@ class Person(models.Model):
     def __str__(self):
         return self.name
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
-    persons = models.ManyToManyField(Person, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
-    
+
+ 
 
