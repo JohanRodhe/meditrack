696f457e06b96d013d63b239324ddcb823627a73
Johan Rodhe <johan.rodhe@decerno.se>
2024-07-16 13:56:51 +0200

    register models for admin site

--- medicine_app/admin.py
+++ medicine_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from accounts.models import User
+from medicine_app.models import Medicine, MedicineEvent, Person
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+
+@admin.register(Medicine)
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = ('name', 'doses', 'current_dose', 'person')
+
+@admin.register(MedicineEvent)
+class MedicineEventAdmin(admin.ModelAdmin):
+    list_display = ('medicine', 'date', 'doses', 'person')
+
 # Register your models here.
+admin.site.register(User)
+
+
+
