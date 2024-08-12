9b129e9113f20934160dcbb22b0cc375f2cdbbcc
Johan Rodhe <johan.rodhe@decerno.se>
2024-08-12 09:09:22 +0200

    change to getenv

--- meditrack/settings.py
+++ meditrack/settings.py
@@ -26,10 +26,10 @@ load_dotenv(ENV_PATH)
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l57nnbvkkm)l67#=b468v7lsam5sf5@zjf-fh9e5kyr2(mzjeu')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-l57nnbvkkm)l67#=b468v7lsam5sf5@zjf-fh9e5kyr2(mzjeu')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'True'
+DEBUG = os.getenv('DJANGO_DEBUG', '') != 'True'
 
 
 ALLOWED_HOSTS = ['meditrack-production.up.railway.app', '127.0.0.1', 'localhost']
